e2dc70b5d97d87922c3a2f25c50a0de259353855
kunalg12 <kunalgursal2923@gmail.com>
2023-06-18 23:11:18 +0530

    updated button

--- components/Button.tsx
+++ components/Button.tsx
@@ -15,8 +15,19 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
     className={twMerge(`
     w-full
     rounded-full
-    
-    `)}
+    bg-green-500
+    border-transparent
+    px-3
+    py-3
+    disabled:cursor-not-allowed
+    disabled:opacity-50
+    text-black
+    font-bold
+    hover:opacity-75
+    transition
+    `,
+    className
+    )}
     >
         {children}
     </button>
